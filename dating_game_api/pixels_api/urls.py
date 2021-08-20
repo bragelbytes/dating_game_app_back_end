03feb3cd5d668973dcd4e5db3cd172c727a6c25d
Ray Hiramatsu <ray@Rays-MacBook-Pro.local>
2021-08-20 10:00:00 -0500

    ray branch filetree cleanup:

--- dating_game_api/pixels_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/users', views.UserList.as_view(), name='user_list'),
-    path('api/users/<int:pk>', views.UserDetail.as_view(), name='user_detail')
-]
