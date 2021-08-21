79f5f6884d3c1f68eecfa9d5da67d5c426ccde66
Doug Moreira <dougmoreira@dougs-mbp.home>
2021-08-21 15:01:21 -0400

    adding space

--- pixels_api/urls.py
+++ pixels_api/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 from django.views.decorators.csrf import csrf_exempt
 
+
+
 urlpatterns = [
     #user path=====
     path('api/useraccount', views.UserList.as_view(), name='user_list'),
