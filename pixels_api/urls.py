ddb44c2acfd78c9e1f0d09d05dc09f0ab8e6d207
Doug Moreira <dougmoreira@dougs-mbp.home>
2021-08-21 13:45:13 -0400

    Auth and Persist Login add to backend

--- pixels_api/urls.py
+++ pixels_api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+from django.views.decorators.csrf import csrf_exempt
+
 urlpatterns = [
     #user path=====
-    path('api/users', views.UserList.as_view(), name='user_list'),
-    path('api/users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
+    path('api/useraccount', views.UserList.as_view(), name='user_list'),
+    path('api/useraccount/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
+    path('api/useraccount/login', csrf_exempt(views.check_login), name="check_login"),
     #games path
     path('api/games', views.GamesList.as_view(), name='games_list'),
     path('api/games/<int:pk>', views.GamesDetail.as_view(), name='games_detail')
--- pixels_api/views.py
+++ pixels_api/views.py
@@ -1,19 +1,43 @@
 #from django.shortcuts import render
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserAccountSerializer
 from .serializers import GamesSerializer
-from .models import User
+from .models import UserAccount
 from .models import Games
+
+from django.contrib.auth.hashers import make_password, check_password
+
+from django.http import JsonResponse
+
+import json
 
 # Create your views here.
 #User views=================
 class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all().order_by('id')
-    serializer_class = UserSerializer
+    queryset = UserAccount.objects.all().order_by('id')
+    serializer_class = UserAccountSerializer
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all().order_by('id')
-    serializer_class = UserSerializer
+    queryset = UserAccount.objects.all().order_by('id')
+    serializer_class = UserAccountSerializer
+def check_login(request):
+
+    if request.method=='GET':
+        return JsonResponse({})
+
+    if request.method=="PUT":
+        
+        jsonRequest = json.loads(request.body)
+        username = jsonRequest['username']
+        password = jsonRequest['password']
+        if UserAccount.objects.get(username=username):
+            user = UserAccount.objects.get(username=username)
+            if check_password(password, user.password):
+                return JsonResponse({'id': user.id, 'username': user.username,})
+            else:
+                return JsonResponse({})
+        else:
+            return JsonResponse({})
 
 #Games views=================
 class GamesList(generics.ListCreateAPIView):
