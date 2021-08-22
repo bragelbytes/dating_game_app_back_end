0f74af6eb21a74189883890da2da5f9ca1601d62
Ray Hiramatsu <ray@Rays-MacBook-Pro.local>
2021-08-22 10:38:55 -0500

    users_liked, many2many

--- pixels_api/models.py
+++ pixels_api/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# # Create your models here.
 class Games(models.Model):
     name = models.CharField(max_length=32)
     image = models.URLField()
     genre = models.CharField(max_length=32)
-
+    # blah = models.CharField(max_length=32)
+#
 class UserAccount(models.Model):
     username = models.CharField(max_length=75, unique=True)
     password = models.CharField(max_length=1000)
@@ -18,3 +19,6 @@ class UserAccount(models.Model):
     # Many to many, Users with Users
     # https://docs.djangoproject.com/en/3.2/ref/models/fields/#django.db.models.ManyToManyField.symmetrical
     users_liked = models.ManyToManyField("self", symmetrical=False, blank=True)
+    
+
+    # blah = models.CharField(max_length=32)
