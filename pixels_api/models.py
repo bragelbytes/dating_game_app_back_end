7a0fd79d7bea62b54dd4c78681dae5d701088241
Ray Hiramatsu <ray@Rays-MacBook-Pro.local>
2021-08-24 08:02:00 -0500

    commented out member variable, users_liked in UserAccount in models.py

--- pixels_api/models.py
+++ pixels_api/models.py
@@ -5,8 +5,7 @@ class Games(models.Model):
     name = models.CharField(max_length=32)
     image = models.URLField()
     genre = models.CharField(max_length=32)
-    # blah = models.CharField(max_length=32)
-#
+
 class UserAccount(models.Model):
     username = models.CharField(max_length=75, unique=True)
     password = models.CharField(max_length=1000)
@@ -17,11 +16,6 @@ class UserAccount(models.Model):
     fav_games = models.ManyToManyField(Games,blank=True)
 
 
-
-
     # Many to many, Users with Users
     # https://docs.djangoproject.com/en/3.2/ref/models/fields/#django.db.models.ManyToManyField.symmetrical
-    users_liked = models.ManyToManyField("self", symmetrical=False, blank=True)
-
-
-    # blah = models.CharField(max_length=32)
+    # users_liked = models.ManyToManyField("self", symmetrical=False, blank=True)
